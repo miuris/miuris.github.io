2b30ddcf1ef67b23b37117969eb14e74014b19a1
Miuris.G <miuris.gutierrez@gmail.com>
2025-07-19 18:08:40 -0400

    Updated html and added styling to css file

---
+++ app.js
@@ -0,0 +1,24 @@
+import background from "./images/pattern-bg-desktopp.png";
+import arrow from ".images/icon-arrow.svg";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+
+
+<section>
+    <div className="absolute -z-10">
+        <img src="/images/pattern-bg-desktop.png" alt="Blueprint layout" className="w-full">
+            IP Address Tracker
+
+            Search for any IP address or domain
+
+            IP Address
+            Location
+            Timezone
+            UTC <!-- add offset value dynamically using the API -->
+            ISP
+
+    </div>
+</section>    
+  
+  
