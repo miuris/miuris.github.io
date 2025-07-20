50e4aa0ea0384c40337b67a2c287eb55250899cb
Miuris.G <miuris.gutierrez@gmail.com>
2025-07-19 23:07:11 -0400

    Updated styling, cleaned up code

--- app.js
+++ app.js
@@ -3,22 +3,5 @@ import arrow from ".images/icon-arrow.svg";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 
-
-
-{/* <section>
-    <div className="absolute -z-10">
-        <img src="/images/pattern-bg-desktop.png" alt="Blueprint layout" className="w-full">
-            IP Address Tracker
-
-            Search for any IP address or domain
-
-            IP Address
-            Location
-            Timezone
-            UTC <!-- add offset value dynamically using the API -->
-            ISP
-
-    </div>
-</section>     */}
   
   
