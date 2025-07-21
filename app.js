0205e8734b034014ad7c3fa86b3adaed4a509a1a
Miuris.G <miuris.gutierrez@gmail.com>
2025-07-20 22:31:07 -0400

    Deployment

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const map = L.map('map').setView([25.7617, 80.1918], 13);
+const map = L.map('map').setView([51.505, -0.09], 13);
 const tileLayer = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
 const attribution = { attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}
 const firstTile=L.tileLayer(tileLayer,attribution)
 firstTile.addTo(map)
-let marker = L.marker([25.7617, 80.1918])
+let marker = L.marker([51.505, -0.09])
 marker.addTo(map)
