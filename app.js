bd00389006821c82f22ac89b385569158dcc765c
Miuris.G <miuris.gutierrez@gmail.com>
2025-07-20 21:27:09 -0400

    Added Geolocation to files

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-import background from "./images/pattern-bg-desktopp.png";
-import arrow from ".images/icon-arrow.svg";
-import L from "leaflet";
-import "leaflet/dist/leaflet.css";
-
-  
-  
+const map = L.map('map').setView([25.7617, 80.1918], 13);
+const tileLayer = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
+const attribution = { attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}
+const firstTile=L.tileLayer(tileLayer,attribution)
+firstTile.addTo(map)
+let marker = L.marker([25.7617, 80.1918])
+marker.addTo(map)
