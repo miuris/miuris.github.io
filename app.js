3498f3f8298d2b141dcc7de7600efb4712ffff47
Miuris.G <miuris.gutierrez@gmail.com>
2025-07-21 12:03:24 -0400

    Replacing with IP Address Tracker Project1

---
+++ app.js
@@ -0,0 +1,9 @@
+const map = L.map('map').setView([51.505, -0.09], 13);
+const tileLayer = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
+const attribution = { attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}
+const firstTile=L.tileLayer(tileLayer,attribution)
+firstTile.addTo(map)
+let marker = L.marker([51.505, -0.09])
+marker.addTo(map)
+
+function
