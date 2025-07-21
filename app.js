a7c30dce0aae22d1df472527c59065293956a055
Miuris Gutierrez <miuris.gutierrez@gmail.com>
2025-07-21 15:03:29 -0400

    Added function for api call. Function 1 out of 3

--- app.js
+++ app.js
@@ -5,3 +5,31 @@ const firstTile=L.tileLayer(tileLayer,attribution)
 firstTile.addTo(map)
 let marker = L.marker([51.505, -0.09])
 marker.addTo(map)
+
+
+/* fetch function*/
+function showIPInfo(ip = '') {
+    const apiKey = 'live_o7ttAKUo3nsruHxzx0rtRwLpBB0D2x6713LJMTwndnwiom17LiHWGfbQWFWaEVBX';
+    const url = `https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}&ipAddress=${ip}`;
+    
+    
+const response = fetch(url);
+const info = response.json();
+
+console.log(info);
+}
+
+
+
+
+
+
+
+
+
+
+/* display function */
+
+
+
+/* eventlistenner function */
