5dd3cdce4602b3f3be3499cb59122ab83033b083
Miuris Gutierrez <miuris.gutierrez@gmail.com>
2025-07-22 21:36:43 -0400

    reset the function task after major app failure

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+const ipaddressInput = document.getElementById('ipaddress-input');
+const arrowBtn = document.getElementById('arrowBtn');
+const ipText = document.getElementById('ip');
+const locationText = document.getElementById('location');
+const timezoneText = document.getElementById('Timezone');
+const ispText = document.getElementById('isp');
 
+let map = L.map('map').setView([35.2923513, -81.5356463], 13);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; OpenStreetMap contributors',
+}).addTo(map);
+let marker;
